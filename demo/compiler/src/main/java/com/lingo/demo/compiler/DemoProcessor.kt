a610281503aab3ab907d11d2e71df41da255c78d
niulinguo <niulinguo@163.com>
2021-06-08 12:46:37 +0800

    apt

--- demo/compiler/src/main/java/com/lingo/demo/compiler/DemoProcessor.kt
+++ demo/compiler/src/main/java/com/lingo/demo/compiler/DemoProcessor.kt
@@ -3,9 +3,12 @@ package com.lingo.demo.compiler
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.RoundEnvironment
 import javax.annotation.processing.SupportedAnnotationTypes
+import javax.annotation.processing.SupportedSourceVersion
+import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
 import javax.tools.Diagnostic
 
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
 @SupportedAnnotationTypes("java.lang.Override")
 class DemoProcessor : AbstractProcessor() {
     override fun process(set: Set<TypeElement>, roundEnvironment: RoundEnvironment): Boolean {
